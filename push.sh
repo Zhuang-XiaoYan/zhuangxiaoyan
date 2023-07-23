5df5333c0ff0df04a6f6b09e86f69612edf21fcf
zhuangxiaoyan <18279148786@163.com>
2023-07-23 12:03:28 +0800

    feat(Zhuang-XiaoYan) [xjl005] 添加提交仓库信息

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 提交gitee仓库信息
+git add .
+git commit -m 'feat(Zhuang-XiaoYan) [xjl005] 添加提交仓库信息'
+git remote push -f origin master:master
+
+# 提交github仓库信息
+git add .
+git commit -m 'feat(Zhuang-XiaoYan) [xjl005] 添加提交仓库信息'
+git remote push -f upstream master:master
+
+cd -
